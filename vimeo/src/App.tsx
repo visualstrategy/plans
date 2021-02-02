f126859fa85d3157bcac0c27ca7bbeea635275fb
Henrik <henrik.svensk@visualstrategy.se>
2021-02-02 11:44:50 +0100

    Compliled

--- vimeo/src/App.tsx
+++ vimeo/src/App.tsx
@@ -9,17 +9,16 @@ function App() {
     <div>
 
       <Panel headerText="Always open (broken)" isOpen={true} >
-        <iframe src="https://player.vimeo.com/video/483030145" width="320" height="173" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen></iframe>
-		/* <iframe src="https://player.vimeo.com/video/483030145" width="320" height="240" allow="autoplay; fullscreen" allowFullScreen></iframe>
-        <DefaultButton text="Open panel" onClick={() => setIsOpen(true)} /> */
+        <iframe src="https://player.vimeo.com/video/483030145" width="320" height="173" frameBorder="0" allow="autoplay; fullscreen; picture-in-picture" allowFullScreen></iframe>
+		{/* <iframe src="https://player.vimeo.com/video/483030145" width="320" height="240" allow="autoplay; fullscreen" allowFullScreen></iframe> */}
+        <DefaultButton text="Open panel" onClick={() => setIsOpen(true)} />
       </Panel>
-<iframe src="https://player.vimeo.com/video/483030145" width="320" height="173" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen></iframe>
       <Panel headerText="Sample panel (works)" isOpen={isOpen}  >
-        /* <iframe src="https://player.vimeo.com/video/483030145" width="320" height="240" allow="autoplay; fullscreen" allowFullScreen></iframe> */
-      </Panel>
+      <iframe src="https://player.vimeo.com/video/483030145" width="320" height="173" frameBorder="0" allow="autoplay; fullscreen; picture-in-picture" allowFullScreen></iframe>
+        {/* <iframe src="https://player.vimeo.com/video/483030145" width="320" height="240" allow="autoplay; fullscreen" allowFullScreen></iframe> */}
+      </Panel>NP
 
     </div>
   );
 }
-
 export default App;
