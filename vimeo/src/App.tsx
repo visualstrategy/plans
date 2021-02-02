cd34ef7686143b171e42df99253883bca6f6aea8
Henrik <henrik.svensk@visualstrategy.se>
2021-02-02 14:02:09 +0100

    allowFullScreen="allowFullScreen"

--- vimeo/src/App.tsx
+++ vimeo/src/App.tsx
@@ -9,8 +9,8 @@ function App() {
     <div>
 
       <Panel headerText="Always open (broken)" isOpen={true} >
-      <iframe width="280" height="157" src="https://www.youtube.com/embed/sMHnpy53de4" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
-      <iframe src="https://player.vimeo.com/video/483030145" width="320" height="173" frameBorder="0" allow="autoplay; fullscreen; picture-in-picture" allowFullScreen></iframe>
+      <iframe width="280" height="157" src="https://www.youtube.com/embed/sMHnpy53de4" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen="allowFullScreen"></iframe>
+      <iframe src="https://player.vimeo.com/video/483030145" width="320" height="173" frameBorder="0" allow="autoplay; fullscreen; picture-in-picture" allowFullScreen="allowFullScreen"></iframe>
       {/* <iframe src="https://player.vimeo.com/video/483030145" width="320" height="240" allow="autoplay; fullscreen" allowFullScreen></iframe> */}
       <DefaultButton text="Open panel" onClick={() => setIsOpen(true)} />
       </Panel>
